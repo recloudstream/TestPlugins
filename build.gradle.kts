ca8afda7b3bead832c54ec0862493bdac188b8d3
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-08-10 21:50:08 +0200

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,8 +48,8 @@ subprojects {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
