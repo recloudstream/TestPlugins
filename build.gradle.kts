f3cf7bc187d6d2ff15b05c45198b131976fe6b1d
deepakcyber46-netizen <deepakcyber46@gmail.com>
2026-08-30 13:15:28 +0530

    Refactor build.gradle.kts for MultiMoviesProvider

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,91 +1,77 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+package com.example
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
-        maven("https://jitpack.io")
-    }
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Document
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-    }
-}
+class MultiMoviesProvider : MainAPI() {
+    override var mainUrl = "https://multimovies.beer"
+    override var name = "MultiMovies"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override var hasMainPage = true
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
+    override suspend fun getMainPage(page: Int, request: HomePageRequest): HomePageResponse? {
+        val html = app.get(mainUrl).text
+        val doc = org.jsoup.Jsoup.parse(html)
+        val homePages = mutableListOf<HomePageList>()
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
-    }
-
-    android {
-        namespace = "com.example"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
+        doc.select("div.items-section, section.featured-box, div.movies-widget").forEach { section ->
+            val headerTitle = section.selectFirst("h2")?.text() ?: "New Additions"
+            val extractedItems = section.select("div.item, div.movie-card").mapNotNull { item ->
+                val title = item.selectFirst("h3")?.text() ?: return@mapNotNull null
+                val itemUrl = item.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+                val posterUrl = item.selectFirst("img")?.attr("src") ?: ""
+                
+                MovieSearchResponse(
+                    name = title,
+                    url = itemUrl,
+                    apiName = this.name,
+                    type = TvType.Movie,
+                    posterUrl = posterUrl
                 )
             }
+            if (extractedItems.isNotEmpty()) {
+                homePages.add(HomePageList(headerTitle, extractedItems))
+            }
+        }
+        return HomePageResponse(homePages)
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchUrl = "$mainUrl/?s=${query.replace(" ", "+")}"
+        val html = app.get(searchUrl).text
+        val doc = org.jsoup.Jsoup.parse(html)
+
+        return doc.select("div.result-item, div.search-result a").mapNotNull { element ->
+            val title = element.selectFirst("h3, div.title")?.text() ?: element.text()
+            val url = element.attr("href") ?: element.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = element.selectFirst("img")?.attr("src") ?: ""
+
+            MovieSearchResponse(
+                name = title,
+                url = url,
+                apiName = this.name,
+                type = TvType.Movie,
+                posterUrl = poster
+            )
         }
     }
 
-    dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (Video) -> Unit
+    ): Boolean {
+        val html = app.get(data).text
+        val doc = org.jsoup.Jsoup.parse(html)
 
-        // Stubs for all cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
-        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
+        doc.select("iframe, video source").forEach { element ->
+            val videoUrl = element.attr("src").takeIf { it.isNotEmpty() } ?: element.attr("src")
+            if (videoUrl.contains("m3u8") || videoUrl.contains("mp4")) {
+                callback.invoke(Video(url = videoUrl, name = "Mirror Stream", referer = mainUrl))
+            }
+        }
+        return true
     }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
